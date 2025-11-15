5b0e29a2d2dead7d592145542fcc0f65e8a9611a
FelipeKosiarz <filip.latka.koszyce@gmail.com>
2025-11-15 20:37:30 +0100

    new walking and camera movement
    new walking and camera movement

--- scenes/players/player1/player.gd
+++ scenes/players/player1/player.gd
@@ -1,12 +1,33 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.005
+#Częstotliwość skakania głowy
+const BOB_FREQ = 2.0
+#Wysokość skoku głowy
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
+#fov
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
 # Camera
 @onready var aim_ray = $CameraController/RayCast3D
+@onready var camera_controller: Node3D = $CameraController
+@onready var camera: Camera3D = $CameraController/Camera3D
 
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		camera_controller.rotate_y(-event.relative.x * SENSITIVITY)
+		camera.rotate_x(-event.relative.y * SENSITIVITY)
+		camera.rotation.x =clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,18 +36,40 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump_1") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("move_left_1", "move_right_1", "move_up_1", "move_down_1")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var direction := (camera_controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x,direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z,direction.z * speed, delta * 7.0)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x,direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z,direction.z * speed, delta * 2.0)
+	#Skakanie głowy
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED *2)
+	var target_fov = BASE_FOV + FOV_CHANGE *velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8)
 	
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y =sin(time * BOB_FREQ) * BOB_AMP 
+	pos.x =cos(time * BOB_FREQ/2) * BOB_AMP
+	return pos
 
 func _shoot_sniper():
 	if aim_ray.is_colliding():
--- scenes/players/player2/player_2.gd
+++ scenes/players/player2/player_2.gd
@@ -1,9 +1,38 @@
 extends CharacterBody3D
 
-const SPEED = 5.0
+var speed
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
+const SENSITIVITY = 0.1
+#Częstotliwość skakania głowy
+const BOB_FREQ = 2.0
+#Wysokość skoku głowy
+const BOB_AMP = 0.08
+var t_bob = 0.0
 
+#fov
+const BASE_FOV = 75.0
+const FOV_CHANGE = 1.5
+# Camera
+@onready var camera_controller: Node3D = $CameraController
+@onready var camera: Camera3D = $CameraController/Camera3D
+
+
+		
 func _physics_process(delta: float) -> void:
+	# Right stick look
+	var look_x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+	var look_y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+	# Deadzone
+	if abs(look_x) < 0.15:
+		look_x = 0
+	if abs(look_y) < 0.15:
+		look_y = 0
+	camera_controller.rotate_y(-look_x * SENSITIVITY * 50.0 * delta)
+	camera.rotate_x(-look_y * SENSITIVITY * 50.0 * delta)
+	camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -11,15 +40,37 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump_2") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+	#Sprint
+	if Input.is_action_pressed("sprint"):
+		speed = SPRINT_SPEED
+	else:
+		speed = WALK_SPEED
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir := Input.get_vector("move_left_2", "move_right_2", "move_up_2", "move_down_2")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var direction := (camera_controller.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if is_on_floor():
+		if direction:
+			velocity.x = direction.x * speed
+			velocity.z = direction.z * speed
+		else:
+			velocity.x = lerp(velocity.x,direction.x * speed, delta * 7.0)
+			velocity.z = lerp(velocity.z,direction.z * speed, delta * 7.0)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		velocity.x = lerp(velocity.x,direction.x * speed, delta * 2.0)
+		velocity.z = lerp(velocity.z,direction.z * speed, delta * 2.0)
+	#Skakanie głowy
+	t_bob += delta * velocity.length() * float(is_on_floor())
+	camera.transform.origin = _headbob(t_bob)
+	
+	#FOV
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED *2)
+	var target_fov = BASE_FOV + FOV_CHANGE *velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8)
 	
 	move_and_slide()
+
+func _headbob(time) -> Vector3:
+	var pos = Vector3.ZERO
+	pos.y =sin(time * BOB_FREQ) * BOB_AMP 
+	pos.x =cos(time * BOB_FREQ/2) * BOB_AMP
+	return pos
