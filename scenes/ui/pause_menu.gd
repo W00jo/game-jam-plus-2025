f1259f983a65903319d54e8b4bfb99a73930ba39
Roksana <roksana.lazarska@gmail.com>
2025-11-16 03:35:03 +0100

    loga

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -1,22 +1,23 @@
 extends Control
 
-var pause_toogle = false
+var pause_toggle = false
 
 func _ready() -> void:
 	self.visible = false
-	
-	
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		print("dziala")
 		pause_and_unpause()
-		
+
 func pause_and_unpause():
-	pause_toogle = !pause_toogle
-	get_tree().paused = pause_toogle
-	self.visible = pause_toogle
+	pause_toggle = !pause_toggle
+	get_tree().paused = pause_toggle
+	self.visible = pause_toggle
 
-
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	self.visible = false
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
